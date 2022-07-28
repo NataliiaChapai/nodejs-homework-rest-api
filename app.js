a8cb11c5042ba1ce20660e4cdaea898ac9e321d2
NataliiaChapai <natalya.chapay@gmail.com>
2022-07-28 13:08:03 +0300

    edit files

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const logger = require('morgan')
 const cors = require('cors')
 require('dotenv').config();
 
-const contactsRouter = require('./routes/api/contacts');
-const usersRouter = require('./routes/api/users');
+const {contactsRouter, usersRouter} = require('./routes/api');
 
 const app = express();
 
--- middlewares/validation.js
+++ middlewares/validation.js
@@ -3,7 +3,7 @@ const validation = (schema) => {
         const { error } = schema.validate(req.body);
         if (error) {
             error.status = 400;
-            return next(error);
+            throw new Error(error.message);
         }
         next();
     }
